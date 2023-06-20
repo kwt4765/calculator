c1ab1f1ddd8fc092c4d66fc1c07156f2a5de47ee
kwt4765 <kwt4765@naver.com>
2023-06-20 10:18:59 +0900

    메시지 박스 추가 git status clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys # 빌트 인 모듈로, 시스템 명령어를 수행할 수 있도록 도와준다.
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox 
+from PyQt5.QtWidgets import (QApplication,
+                            QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) 
 
 from PyQt5.QtGui import QIcon
 #Qwidget을 기반으로 한 클래스를 설계하여 추후 객체 생성하도록 하겠다!
@@ -11,11 +12,14 @@ class Calculator(QWidget) :
         self.initUI()
 
     def initUI(self) :
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
         
         vbox=QVBoxLayout()
-        vbox.addStretch(1)
+        vbox.addWidget(self.te1)
         vbox.addWidget(self.btn1)
         vbox.addStretch(1)
 
@@ -27,7 +31,7 @@ class Calculator(QWidget) :
         self.show()
 
     def activateMessage(self) :
-        QMessageBox.information(self,"Information","button activated!")
+        self.te1.appendPlainText("버튼이 눌렸다옹~~")
 
 #이 파일을 직접 실행할 시에만 명령을 수행하겠다!    
 if __name__=='__main__' :
