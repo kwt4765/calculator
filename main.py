c61cc4e797490dddb90bbf61bff98ce711cbf4a5
kwt4765 <kwt4765@naver.com>
2023-06-20 09:36:17 +0900

    책에는 없지만, 주석을 추가하기 위해 맘대로 커밋을 추가했어요.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sys
+import sys # 빌트 인 모듈로, 시스템 명령어를 수행할 수 있도록 도와준다.
 from PyQt5.QtWidgets import QApplication, QWidget
-class Calculator(QWidget) :
 
+#Qwidget을 기반으로 한 클래스를 설계하여 추후 객체 생성하도록 하겠다!
+class Calculator(QWidget) : 
+
+    #super() : 클래스가 어떤 객체를 기반으로 만들어 질 때, 그 기반 객체를 뜻한다.
     def __init__(self) :
         super().__init__()
         self.initUI()
@@ -10,9 +13,10 @@ class Calculator(QWidget) :
         self.setWindowTitle('Calculator')
         self.resize(256,256)
         self.show()
-    
+
+#이 파일을 직접 실행할 시에만 명령을 수행하겠다!    
 if __name__=='__main__' :
-    app = QApplication(sys.argv)
-    view = Calculator()
-    sys.exit(app.exec_())
+    app = QApplication(sys.argv) # 시스템에서 주어지는 인수를 받아 앱을 켜겠다.
+    view = Calculator() # 위에서 만든 칼큘레이터 객체를 생성하겠다.
+    sys.exit(app.exec_()) # 창이 꺼질 때 시스템도 끄겠다.
 
