6671b788bd9b91edafedd72294f7d06f1235e31e
kwt4765 <kwt4765@naver.com>
2023-06-20 11:34:11 +0900

    로또 기능과 clear 기능을 추가했어요

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys # 빌트 인 모듈로, 시스템 명령어를 수행할 수 있도록 도와준다.
 from PyQt5.QtWidgets import (QApplication,
-                            QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit) 
+                            QWidget, QPushButton, QVBoxLayout, 
+                            QMessageBox, QPlainTextEdit, QHBoxLayout) 
+
+import random # 내 맘대로 로또 추첨 기능 추가할래
 
 from PyQt5.QtGui import QIcon
 #Qwidget을 기반으로 한 클래스를 설계하여 추후 객체 생성하도록 하겠다!
@@ -18,9 +21,17 @@ class Calculator(QWidget) :
         self.btn1 = QPushButton('Message', self)
         self.btn1.clicked.connect(self.activateMessage)
         
+        self.btn2 = QPushButton('Clear', self)
+        self.btn2.clicked.connect(self.clearMessage)
+
+        hbox=QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+
         vbox=QVBoxLayout()
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
 
         self.setLayout(vbox)
@@ -31,7 +42,11 @@ class Calculator(QWidget) :
         self.show()
 
     def activateMessage(self) :
-        self.te1.appendPlainText("버튼이 눌렸다옹~~")
+        lottoNum = random.sample(range(1, 46), 6)
+        self.te1.appendPlainText(str(lottoNum))
+    
+    def clearMessage(self) :
+        self.te1.clear()
 
 #이 파일을 직접 실행할 시에만 명령을 수행하겠다!    
 if __name__=='__main__' :
