60987b327890c1352986bf0b281d79e21a3ac884
kwt4765 <kwt4765@naver.com>
2023-06-21 09:44:14 +0900

    모듈 하나 굳이 추가해 봄

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ import random # 내 맘대로 로또 추첨 기능 추가할래
 
 from PyQt5.QtGui import QIcon
 #Qwidget을 기반으로 한 클래스를 설계하여 추후 객체 생성하도록 하겠다!
+
+from prompt import sayHello
+
+
 class Calculator(QWidget) : 
 
     #super() : 클래스가 어떤 객체를 기반으로 만들어 질 때, 그 기반 객체를 뜻한다.
@@ -50,6 +54,7 @@ class Calculator(QWidget) :
 
 #이 파일을 직접 실행할 시에만 명령을 수행하겠다!    
 if __name__=='__main__' :
+    sayHello()
     app = QApplication(sys.argv) # 시스템에서 주어지는 인수를 받아 앱을 켜겠다.
     view = Calculator() # 위에서 만든 칼큘레이터 객체를 생성하겠다.
     sys.exit(app.exec_()) # 창이 꺼질 때 시스템도 끄겠다.
